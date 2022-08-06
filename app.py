50ff542ea65f11494e159027261b109bd87e73ff
hasferrr <hasbi.f.hamd@gmail.com>
2022-08-06 17:08:22 +0700

    finish app.py

--- app.py
+++ app.py
@@ -3,16 +3,12 @@ from random import random
 
 
 def main():
-    # Check validity file .csv
 
     # Open file
     csv_file = open("test.csv")
-
     lines = csv.reader(csv_file)
-    for line in lines:
-        print(line)
 
-    # Write file xml
+    # Open file xml
     with open("output.xml", "w") as output:
 
         # Write header
@@ -24,7 +20,35 @@ def main():
     <EditionFlagDefault>1</EditionFlagDefault>
     <EditionUID>""")
         output.write(str(int(random()*10**18)))
-        output.write("</EditionUID>")
+        output.write("</EditionUID>\n")
+
+        # Write ChapterAtom
+        chapter_uid = 3000000000
+        for line in lines:
+
+            # Opening
+            output.write("    <ChapterAtom>\n")
+
+            # ChapterUID and ChapterTimeStart
+            chapter_uid += 1
+            output.write(f"      <ChapterUID>{chapter_uid}</ChapterUID>\n")
+            output.write(f"      <ChapterTimeStart>{line[0]}.000000000</ChapterTimeStart>\n")
+
+            # ChapterDisplay
+            output.write("      <ChapterDisplay>\n")
+            output.write(f"        <ChapterString>{line[1]}</ChapterString>\n")
+            output.write(
+"""        <ChapterLanguage>und</ChapterLanguage>
+        <ChapLanguageIETF>und</ChapLanguageIETF>
+      </ChapterDisplay>\n""")
+
+            # Ending
+            output.write("    </ChapterAtom>\n")
+
+        # Write footer
+        output.write(
+"""  </EditionEntry>
+</Chapters>\n""")
 
 
     # Close file
